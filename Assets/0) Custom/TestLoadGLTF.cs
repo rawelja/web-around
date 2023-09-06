5771f881ce71eea1b2685d2df5d5cc074dd69089
Ravic Bojan <ravicbojan@ymail.com>
2023-09-06 09:39:12 +0200

    Init

---
+++ Assets/0) Custom/TestLoadGLTF.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLoadGLTF : MonoBehaviour
+{
+    public string modelUrl = "https://echo.ix42.com/stegosaur.glb";
+    public void LoadModelTest()
+    {
+        var gltf = gameObject.AddComponent<GLTFast.GltfAsset>();
+        //gltf.Url = "https://echo.ix42.com/stegosaurs_SStenops.gltf";
+        gltf.Url = modelUrl;
+    }
+}
